fc1a118eee2a2318ebf98dd162eb6e8dba17d0cc
MartinVacheron <martinvdu92@gmail.com>
2025-07-10 23:22:08 +0200

    Windows REPL wip

--- src/runtime/Repl.zig
+++ src/runtime/Repl.zig
@@ -2,8 +2,10 @@ const std = @import("std");
 const Allocator = std.mem.Allocator;
 const Prompts = std.ArrayListUnmanaged([]const u8);
 
+const Tty = @import("Tty.zig");
 const oom = @import("../utils.zig").oom;
 
+tty: Tty,
 stdin: std.fs.File.Reader,
 stdout: std.fs.File.Writer,
 prompts: Prompts = .{},
@@ -17,12 +19,15 @@ const Error = error{ BadRead, BadWrite, TooManyIndents, Empty, EndOfFile };
 
 pub fn init() Self {
     return .{
+        .tty = Tty.init() catch unreachable,
         .stdin = std.io.getStdIn().reader(),
         .stdout = std.io.getStdOut().writer(),
     };
 }
 
 pub fn deinit(self: *Self, allocator: Allocator) void {
+    self.tty.deinit();
+
     for (self.prompts.items) |prompt| {
         allocator.free(prompt);
     }
@@ -34,31 +39,41 @@ pub fn logInfos(self: *Self) Error!void {
 }
 
 pub fn getPrompt(self: *Self, allocator: Allocator) Error![:0]const u8 {
-    var input: std.ArrayListUnmanaged(u8) = .{};
-    errdefer input.deinit(allocator);
+    _ = allocator;
 
-    const prev_level = self.indent_level;
-    while (true) {
-        const prev_written = input.items.len;
-        try self.printPs1();
-        self.stdin.streamUntilDelimiter(input.writer(allocator), '\n', null) catch return error.BadRead;
-        try self.checkIndent(input.items[prev_written..]);
+    const event = self.tty.getEvent() catch return error.EndOfFile;
+    _ = event; // autofix
 
-        if (self.indent_level == prev_level) break;
-    }
+    // const prompt = self.tty.getPrompt() catch return error.EndOfFile;
+    // _ = prompt; // autofix
 
-    if (input.items.len == 1) {
-        return error.Empty;
-    } else if (input.items.len >= 4 and std.mem.eql(u8, input.items[0..4], "quit")) {
-        return error.EndOfFile;
-    }
+    return "";
 
-    input.append(allocator, '\n') catch oom();
-    input.append(allocator, 0) catch oom();
-    const len = input.items.len - 1;
-    self.prompts.append(allocator, input.toOwnedSlice(allocator) catch oom()) catch oom();
+    // var input: std.ArrayListUnmanaged(u8) = .{};
+    // errdefer input.deinit(allocator);
 
-    return self.prompts.items[self.prompts.items.len - 1][0..len :0];
+    // const prev_level = self.indent_level;
+    // while (true) {
+    //     const prev_written = input.items.len;
+    //     try self.printPs1();
+    //     self.stdin.streamUntilDelimiter(input.writer(allocator), '\n', null) catch return error.BadRead;
+    //     try self.checkIndent(input.items[prev_written..]);
+    //
+    //     if (self.indent_level == prev_level) break;
+    // }
+    //
+    // if (input.items.len == 1) {
+    //     return error.Empty;
+    // } else if (input.items.len >= 4 and std.mem.eql(u8, input.items[0..4], "quit")) {
+    //     return error.EndOfFile;
+    // }
+    //
+    // input.append(allocator, '\n') catch oom();
+    // input.append(allocator, 0) catch oom();
+    // const len = input.items.len - 1;
+    // self.prompts.append(allocator, input.toOwnedSlice(allocator) catch oom()) catch oom();
+    //
+    // return self.prompts.items[self.prompts.items.len - 1][0..len :0];
 }
 
 fn printPs1(self: *Self) Error!void {
